633aebc3d579b1246b8d98740171faa6e8616a11
Andryu <andryuprog@gmail.com>
2024-05-23 00:10:00 +0300

    refactor

--- lib/data/utilities/constants.dart
+++ lib/data/utilities/constants.dart
@@ -1,10 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Constants {
-
-     static late final WEATHER_APP_ID;
-
-  // static const String WEATHER_APP_ID = '';
+  static late final WEATHER_APP_ID;
   static const String WEATHER_BASE_SCHEME = 'https://';
   static const String WEATER_BASE_URL_DOMAIN = 'api.openweathermap.org';
   static const String WEATHE_FORECAST_PATH = '/data/2.5/forecast';
@@ -12,8 +9,7 @@ class Constants {
   static const String WEATHE_IMAGES_URL =
       WEATHER_BASE_SCHEME + WEATER_BASE_URL_DOMAIN + WEATHE_IMAGES_PATH;
 
-  static void loadEnv(){
+  static void loadEnv() {
     WEATHER_APP_ID = dotenv.env['OPENWEATHERMAP_API_KEY'] ?? '';
   }
-
 }
