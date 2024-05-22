017b1d34792b3396c3c2694b0456b66033384650
Andryu <andryuprog@gmail.com>
2024-05-22 21:38:50 +0300

    refactor

---
+++ lib/data/repositories/authorization_repository.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class AuthorizationRepository {
+  AuthorizationRepository(this.sharedPreferences);
+    SharedPreferences sharedPreferences;
+    String namePrefKey = 'name_pref';
+    String passwordPrefKey = 'password_pref';
+    String removePrefKey = 'remove_pref';
+
+
+   Future<void> setNamePref(String value) async {
+    await sharedPreferences.setString(namePrefKey, value);// метод записывает в базу
+    getNamePref();
+   }
+
+  Future<void> setPasswordPref(String value) async {
+    await sharedPreferences.setString(passwordPrefKey, value);
+    getPasswordPref();
+  }
+
+  String? getNamePref() {
+    return sharedPreferences.getString(namePrefKey);// считывает данные с базы
+
+  }
+
+  String? getPasswordPref() {
+     return sharedPreferences.getString(passwordPrefKey);// считывает данные с базы
+  }
+
+  Future deletePreferences() async {
+   await sharedPreferences.remove(namePrefKey);
+   await sharedPreferences.remove(passwordPrefKey);
+  }
+
+
+  }
---
+++ lib/data/repositories/weather_repository.dart
@@ -0,0 +1,20 @@
+
+import '../../data/api/weather_dio_api.dart';
+import '../../data/model/weather_forecast_daily.dart';
+
+
+
+const String cityName = 'Dnipro';
+
+class WeatherRepository {
+
+      final WeatherApi weatherApi;
+
+      WeatherRepository (this.weatherApi);
+
+   Future<WeatherForecast> forecastObject() async {
+ return WeatherApi().fetchWeatherForecastWithCity(cityName: cityName);
+ }
+
+
+}
